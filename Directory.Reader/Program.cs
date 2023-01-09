13dfd2312040707471295ba4a13ee7f88c298feb
Vladimir Makarevich <makarevich.dev@outlook.com>
2023-01-09 12:40:04 +0100

    update search and add .md format export

--- Directory.Reader/Program.cs
+++ Directory.Reader/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -7,18 +8,36 @@ using System.Text;
 
 namespace Directories.Reader
 {
+    public class ContainerModel
+    {
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public List<ContainerModel> SubContainers { get; set; }
+        public List<FileModel> Files { get; set; }
+    }
+
+    public class FileModel
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+    }
+
     public class Program
     {
+        public static List<ContainerModel> Directories = new List<ContainerModel>();
+
         public static void Main(string[] args)
         {
             var collections = args.ToList();
 
-            collections.Add("d:\\");
-            collections.Add("e:\\");
-            collections.Add("f:\\");
-            collections.Add("g:\\");
-            collections.Add("h:\\");
-            collections.Add("j:\\");
+            //collections.Add("d:\\");
+            //collections.Add("e:\\");
+            //collections.Add("f:\\");
+            //collections.Add("g:\\");
+            //collections.Add("h:\\");
+            //collections.Add("j:\\");
+
+            collections.Add("C:\\$VladimirMakarevich");
 
             var datetime = DateTime.Now.ToString("yyyy-dd-M-HH-mm", CultureInfo.InvariantCulture);
 
@@ -26,45 +45,116 @@ namespace Directories.Reader
 
             foreach (string path in collections)
             {
-                if (File.Exists(path))
+                if (Directory.Exists(path))
                 {
-                    // This path is a file
-                    ProcessFile(path);
-                }
-                else if (Directory.Exists(path))
-                {
-                    // This path is a directory
-                    ProcessDirectory(path);
+                    // This path is a container
+                    var container = new ContainerModel();
+                    container.Path = path;
+                    container.Name = GetNameFromPath(path);
+
+                    ProcessDirectory(path, container);
+
+                    Directories.Add(container);
                 }
                 else
                 {
-                    Console.WriteLine("{0} is not a valid file or directory.", path);
+                    Console.WriteLine("{0} is not a valid file or container.", path);
                 }
             }
 
-            File.WriteAllText($"$directories-{datetime}.md", _sbDirectories.ToString());
-            File.WriteAllText($"$paths-{datetime}.md", _sbPaths.ToString());
             File.WriteAllText($"$errors-{datetime}.md", _sbErrors.ToString());
+            File.WriteAllText($"json-notes-{datetime}.md", JsonConvert.SerializeObject(Directories, Formatting.Indented));
+
+            ProcessMdFormat(datetime);
         }
 
-        // Process all files in the directory passed in, recurse on any directories
-        // that are found, and process the files they contain.
-        public static void ProcessDirectory(string targetDirectory)
+        private static void ProcessMdFormat(string datetime)
         {
-            if (IgnoreList.Contains(targetDirectory.Split('\\').Last()))
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var container in Directories)
+            {
+                var nestingCount = 0;
+                sb.AppendLine($"# =={container.Name}==");
+                sb.AppendLine("");
+                sb.AppendLine($"> {container.Path}.");
+                sb.AppendLine("");
+                sb.AppendLine("## SystemInfo:");
+                sb.AppendLine($"User Domain Name: {Environment.UserDomainName}.");
+                sb.AppendLine($"User Name: {Environment.UserName}.");
+                sb.AppendLine($"Version: {Environment.Version}.");
+                sb.AppendLine("");
+                sb.AppendLine("");
+
+                if (container.Files != null && container.Files.Any())
+                {
+                    ProcessFilesMdFormat(container.Files, sb, nestingCount);
+                }
+
+                if (container.SubContainers != null && container.SubContainers.Any())
+                {
+                    ProcessSubContainersMdFormat(container.SubContainers, sb, nestingCount);
+                }
+            }
+
+            File.WriteAllText($"notes-{datetime}.md", sb.ToString());
+        }
+
+        private static void ProcessSubContainersMdFormat(List<ContainerModel> subContainers, StringBuilder sb, int nestingCount)
+        {
+            foreach (var container in subContainers)
+            {
+                sb.AppendLine($"{GetNestingWhitespaces(nestingCount)}- !{container.Name}");
+                if (container.SubContainers != null && container.SubContainers.Any())
+                {
+                    if (container.Files != null && container.Files.Any())
+                    {
+                        ProcessFilesMdFormat(container.Files, sb, nestingCount + 1);
+                    }
+
+                    if (container.SubContainers != null && container.SubContainers.Any())
+                    {
+                        ProcessSubContainersMdFormat(container.SubContainers, sb, nestingCount + 1);
+                    }
+                }
+            }
+        }
+
+        private static void ProcessFilesMdFormat(List<FileModel> containerFiles, StringBuilder sb, int nestingCount)
+        {
+            foreach (var containerFile in containerFiles)
+            {
+                sb.AppendLine($"{GetNestingWhitespaces(nestingCount)}- `{containerFile.Name}`");
+            }
+        }
+
+        public static string GetNestingWhitespaces(int nestingCount)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < nestingCount; i++)
+            {
+                sb.Append("\t");
+            }
+
+            return sb.ToString();
+        }
+
+        // Process all files in the container passed in, recurse on any directories
+        // that are found, and process the files they contain.
+        public static void ProcessDirectory(string targetDirectory, ContainerModel container)
+        {
+            if (IgnoreList.Contains(GetNameFromPath(targetDirectory)))
             {
                 return;
             }
 
-            _sbDirectories.AppendLine($"`{targetDirectory}`");
-
-            // Process the list of files found in the directory.
+            // Process the list of files found in the container.
             try
             {
                 string[] fileEntries = Directory.GetFiles(targetDirectory);
                 foreach (string fileName in fileEntries)
                 {
-                    ProcessFile(fileName);
+                    ProcessFile(fileName, container);
                 }
             }
             catch (Exception e)
@@ -72,13 +162,21 @@ namespace Directories.Reader
                 _sbErrors.AppendLine($"`{e.Message}`");
             }
 
-            // Recurse into subdirectories of this directory.
+            // Recurse into subdirectories of this container.
             try
             {
                 string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
                 foreach (string subdirectory in subdirectoryEntries)
                 {
-                    ProcessDirectory(subdirectory);
+                    var subContainer = new ContainerModel()
+                    {
+                        Name = GetNameFromPath(subdirectory)
+                    };
+
+                    container.SubContainers ??= new List<ContainerModel>();
+                    container.SubContainers.Add(subContainer);
+
+                    ProcessDirectory(subdirectory, subContainer);
                 }
             }
             catch (Exception e)
@@ -87,8 +185,13 @@ namespace Directories.Reader
             }
         }
 
+        private static string GetNameFromPath(string targetDirectory)
+        {
+            return targetDirectory.Split("\\").Last();
+        }
+
         // Insert logic for processing found files here.
-        public static void ProcessFile(string path)
+        public static void ProcessFile(string path, ContainerModel container)
         {
             FileInfo fileInfo = new FileInfo(path);
             if (!Extensions.Contains(fileInfo.Extension))
@@ -97,11 +200,15 @@ namespace Directories.Reader
             }
 
             Console.WriteLine("Processed file '{0}'.", path);
-            _sbPaths.AppendLine($"`{path}`");
+
+            container.Files ??= new List<FileModel>();
+            container.Files.Add(new FileModel()
+            {
+                Name = fileInfo.Name,
+                Extension = fileInfo.Extension
+            });
         }
 
-        private static StringBuilder _sbDirectories = new StringBuilder();
-        private static StringBuilder _sbPaths = new StringBuilder();
         private static StringBuilder _sbErrors = new StringBuilder();
 
         private static readonly List<string> IgnoreList = new List<string>()
@@ -131,7 +238,10 @@ namespace Directories.Reader
             ".rar",
             ".iso",
             ".exe",
-            ".mp4"
+            ".mp4",
+            ".epub",
+            ".fb2",
+            ".m4v"
         };
     }
 }
